8de854b51d98f4114a8a85e5b42bef98cca88d2f
alvinliu20 <102318062+alvinliu20@users.noreply.github.com>
2022-03-24 10:57:34 -0700

    Update ReservationsMain.java

--- src/ReservationsMain.java
+++ src/ReservationsMain.java
@@ -1,5 +1,8 @@
 class ReservationsMain {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        // on startup
+        
+        // running from a text menu
     }
 }
