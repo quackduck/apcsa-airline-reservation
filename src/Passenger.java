c4db10e5e1f7a7835a1d850dce9341a0d0584950
alvinliu20 <102318062+alvinliu20@users.noreply.github.com>
2022-03-24 11:00:01 -0700

    Update Passenger.java

--- src/Passenger.java
+++ src/Passenger.java
@@ -2,5 +2,10 @@ class Passenger {
     public static void main(String[] args) {
         // instance variables
         private String lastName;
+        
+        // constructor
+        public Passenger(String firstName, String lastName) {
+            
+        }
     }
 }
