deae06818a16689404d3e09f77456c66ec3123fb
alvinliu20 <102318062+alvinliu20@users.noreply.github.com>
2022-03-24 11:03:03 -0700

    Update ReservationsMain.java

--- src/ReservationsMain.java
+++ src/ReservationsMain.java
@@ -2,6 +2,8 @@ class ReservationsMain {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         // on startup
+        ArrayList<Airport> airports = new ArrayList<Airport>;
+        
         
         // running from a text menu
     }
