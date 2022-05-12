d3a26090f5e77c16df46d66fd9cb39cb46bce447
nbenabbas <102318054+nbenabbas@users.noreply.github.com>
2022-05-12 11:04:22 -0700

    Update Passenger.java

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,11 +1,20 @@
-class Passenger {
-    public static void main(String[] args) {
-        // instance variables
-        private String lastName;
-        
-        // constructor
-        public Passenger(String firstName, String lastName) {
-            
-        }
+public class Passenger {
+    private String firstName; 
+    private String lastName;
+    
+    public Passenger(String first, String last) {
+        firstName = first;
+        lastName = last;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String toString() {
+        return firstName + " " + lastName;
     }
 }
+
