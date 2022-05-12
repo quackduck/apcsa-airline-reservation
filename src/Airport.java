fbdd158900e3d716b3c4e1d27577513459e86ae5
riyaraghu13 <102318051+riyaraghu13@users.noreply.github.com>
2022-05-12 10:48:27 -0700

    Update Airport.java

--- src/Airport.java
+++ src/Airport.java
@@ -3,15 +3,17 @@ public class Airport {
    private String airport;
    private String city;
    private String state;
+   private double GMTOffset;
    
-   public Airport(String airport, String city, String state) {
+   public Airport(String airport, String city, String state, double GMTOffset) {
       this.airport = airport;
       this.city = city;
       this.state = state;
+      this.GMTOffset = GMTOffset;
    }
    
    public String toString() {
-      String str = "Airport: " + airport + "\nCity: " + city + "\nState: " + state; 
+      String str = "Airport: " + airport + "\nCity: " + city + "\nState: " + state + "\nGMT Offset: " + GMTOffset; 
       return str;
    }
 
