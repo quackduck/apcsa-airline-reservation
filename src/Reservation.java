ad9296d0874240f99a5542ba94cbf6f16e34b3b7
chidiya21 <102318069+chidiya21@users.noreply.github.com>
2022-05-18 12:40:06 -0700

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,5 +1,33 @@
-class Reservation {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+/*
+ * Reservation.java
+ * by Nikki Benabbas
+ * 5/17/2022
+ * p3
+ */
+
+package ARS;
+import java.util.ArrayList;
+
+public class Reservation {
+   private ArrayList <Passenger> passengers;
+   private Flight flight;
+   private int reservationNumber;
+
+   public Reservation(ArrayList <Passenger> passengers, Flight flight, int reservationNumber) {
+      this.passengers = passengers;
+      this.flight = flight;
+      this.reservationNumber = reservationNumber;
+   }
+     
+   public ArrayList <Passenger> getPassengersList() {
+      return passengers;
+   }
+
+   public Flight getFlight() {
+      return flight;
+   }
+
+   public int getReservationNumber() {
+      return reservationNumber;
+   }
 }
