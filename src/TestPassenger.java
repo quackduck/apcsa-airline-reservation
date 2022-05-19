20c9383446e7a477edc5a4eccc405d239f7c4b83
alvinliu20 <102318062+alvinliu20@users.noreply.github.com>
2022-05-19 09:49:28 -0700

    Create TestPassenger.java

---
+++ src/TestPassenger.java
@@ -0,0 +1,55 @@
+// TestPassenger.java
+// by Alvin Liu
+// 5/19/2022
+// Period 3
+
+import java.util.Scanner;
+
+public class TestPassenger {
+
+   public static void main(String[] args) {
+   
+      ArrayList<Passenger> passengers = new ArrayList<Passenger>;
+      
+      Scanner s = new Scanner(System.in);
+      
+      String selection = "";
+      
+      while (!selection.equals("Q")) {
+         System.out.println("Menu options:");
+         System.out.println("A: Print passengers"):
+         System.out.println("B: Add passenger");
+         System.out.println("C: Remove passenger");
+         System.out.println("Q: Quit");
+         
+         System.out.print("Enter A, B, C, or Q: ");
+         selection = s.next();
+         selection = selection.toUpperCase();
+         System.out.println();
+         
+         if (selection.equals("A")) {
+            for (Passenger p: passengers) {
+               System.out.println(p);
+            }
+         } else if (selection.equals("B")) {
+            System.out.println("Enter passenger first name: ");
+            String firstName = s.next();
+            System.out.println("Enter passenger last name: ");
+            String lastName = s.next();
+            passengers.add(new Passenger(first, last));
+         } else if (selection.equals("C")) {
+            System.out.println("Enter first name of passenger to be removed: ");
+            String firstName = s.next();
+            System.out.println("Enter last name of passenger to be removed: ");
+            String lastName = s.next();
+            for (int i = 0; i < passengers.size(); i++) {
+               if (passengers.get(i).getFirstName().equals(firstName) && passengers.get(i).getLastName().equals(lastName)) {
+                  passengers.remove(i);
+                  break;
+               }
+            }
+         }
+      }
+   }
+
+}
